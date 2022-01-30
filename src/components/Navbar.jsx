99113bb8b9e7af53817817b04bbfb896ce3f4939
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-01-30 15:47:21 -0800

    Add logo and menu items to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
 
-import icon from '../images/cryptocurrency.png'
+import icon from '../images/logo.png'
 const Navbar = () => {
   return (
   <div className="nav-container">
@@ -16,6 +16,20 @@ const Navbar = () => {
         {/* Controls menu for mobile devices */} 
         {/* <Button className="menu-control-container"></Button> */}
       </div>
+      <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined/>}>
+              <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined/>}>
+              <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined/>}>
+              <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined/>}>
+              <Link to="/news">News</Link>
+          </Menu.Item>
+      </Menu>
   </div>
   );
 }; 
