2bb5cf172a25ec7052eb88a1f87d79d04f764608
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-15 09:29:25 -0800

    Create cryptoNewsApi and create fetch request to Bing News API

---
+++ src/services/cryptoNewsApi.js
@@ -0,0 +1,29 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit';
+
+const cryptoNewsHeaders = {
+    'x-bingapis-sdk': 'true',
+    'x-rapidapi-host': 'bing-news-search1.p.rapidapi.com',
+    'x-rapidapi-key': '45dc652efbmshca6514dd5735400p17a824jsn755cde182ad4'
+}
+
+const baseUrl = 'https://bing-news-search1.p.rapidapi.com'
+
+const createRequest = (url) => ({ url, headers: cryptoNewsHeaders })
+
+export const cryptoNewsApi = createApi({
+    reducerPath: 'cryptoNewsApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptoNews: builder.query({
+            query: ({ newsCategory, count }) => 
+            // parameters from bing news API
+            createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+        })
+    })
+});
+
+export const { 
+    // Redux toolkit Hook: "use<EndpointProp>Query"
+    useGetCryptoNewsQuery 
+}
+    = cryptoNewsApi;
