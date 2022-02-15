584063025b0e295a7740ae57fcc591cd58162303
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-15 14:49:42 -0800

    Display description of news article, last updated, and news provider in each Card component of news.

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -7,6 +7,8 @@ import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
 const { Text, Title } = Typography;
 const { Option } = Select;
 
+const demoImage = 'http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg';
+
 const News = ({simplified}) => {
 
   // Fetch crypto news
@@ -15,6 +17,8 @@ const News = ({simplified}) => {
     // we defined two variables (newsCategory, count) in our query in cryptoNewsApi
     useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 })
 
+
+    console.log(cryptoNews)
   // if no news
   if (!cryptoNews?.value) return 'Loading...';
 
@@ -28,7 +32,23 @@ const News = ({simplified}) => {
             <a href={news.url} target="_blank" rel="norefferer">
               <div className="news-image-container">
                 <Title className="news-title" level={4}>{news.name}</Title>
-
+                  {/* Display news article image or default image if none */}
+                  <img style={{ maxWidth: '200px', maxHeight: '100px'}} src={news?.image?.thumbnail?.contentUrl || demoImage } alt="news"/>
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0,100)}...`
+                  : news.description
+                }
+              </p>
+              <div className="provider-container">
+                <div>
+                  <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl || demoImage} alt="news" />
+                  {/* Display name of news provider */}
+                  <Text className="provider-name">{news.provider[0]?.name}</Text>
+                </div>
+                  {/*  Display when news article was posted */}
+                  <Text>{moment(news.datePublished).startOf('ss').fromNow()}</Text>
               </div>
             </a>
           </Card>
