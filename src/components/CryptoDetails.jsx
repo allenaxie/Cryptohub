2f42d568c347c1394095befcd3561ad397e57d58
Allen Xie <allenxie@Allens-MacBook-Pro.local>
2022-02-15 23:01:57 -0800

    Add link in crypto details page

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -117,8 +117,18 @@ const CryptoDetails = () => {
           </Row>
           <Col className="coin-links">
             <Title level={3} className="coin-details-heading">
-
+              {cryptoDetails.name} Links
             </Title>
+            {cryptoDetails.links.map((link) => (
+              <Row className="coin-link" key={link.name}>
+                <Title level={5} className="link-name">
+                  {link.type}
+                </Title>
+                <a href={link.url} target="_blank" rel="noreferrer">
+                  {link.name}
+                </a>
+              </Row>
+            ))}
           </Col>
         </Col>
       </Col>
